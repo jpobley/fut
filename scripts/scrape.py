cd0a19fadf630e1d5efd72edeb7fe90b7920c43a
JP Obley <jpobley@gmail.com>
2016-01-09 19:14:26 -0500

    update scrape and ingore

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
-import urllib, json
+import urllib, json, datetime, os
+
 from time import sleep
+from config import files
 
-#url = 'https://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:{},%22position%22:%22LF,CF,RF,ST,LW,LM,CAM,CDM,CM,RM,RW,LWB,LB,CB,RB,RWB%22%7D'
-url = 'https://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:{},%22position%22:%22GK%22%7D'
+today = datetime.datetime.today().strftime("%Y%m%d")
+data_folder = '../data/{}'.format(today)
 
-print "Loading page #{}...".format(1)
-print
-response = urllib.urlopen(url.format(1))
-data = json.loads(response.read())
-total_pages = data['totalPages']
-items = data.get('items', [])
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+else:
+    import random
+    while os.path.exists(data_folder);
+        data_folder += "_" + random.randint(0,100)
+    os.makedirs(data_folder)
 
-print "Loaded page #{} items. Current count: {} players".format(1, len(items))
-print
+for name in files.keys():
+    filename = '{}.json'.format(name)
+    filepath = '{}/{}'.format(data_folder, filename)
 
-for moose in range(2, total_pages+1):
-    sleep(.5)
-    print "Loading page #{}...".format(moose)
-    print
-    response = urllib.urlopen(url.format(moose))
+    #get first page
+    response = urllib.urlopen(files[name].format(1))
     data = json.loads(response.read())
-    items += data.get('items', [])
-    print "Loaded page #{} items. Current count: {} players".format(moose, len(items))
-    print
+    total_pages = data['totalPages']
+    items = data.get('items', [])
 
-with open('keepers.json', 'wb') as out:
-    out.write(json.dumps(items))
+    for i in range(2, total_pages+1):
+        sleep(.5)
+        response = urllib.urlopen(url.format(i))
+        data = json.loads(response.read())
+        items += data.get('items', [])
+
+    with open(filepath, 'wb') as out:
+        out.write(json.dumps(items))
